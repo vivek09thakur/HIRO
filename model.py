58e5635c8ab4b408798f0311fd3123cb32643e76
Vivek Thakur <20509.vivek@gpranchi.org>
2023-08-06 11:20:06 +0530

    commit

--- model.py
+++ model.py
@@ -1,1 +1,9 @@
+import csv
+import re
+import numpy as np
+import pandas
+from sklearn import preprocessing
+from sklearn.model_selection import cross_val_score,train_test_split
+from sklearn.tree import DecisionTreeClassifier,_tree
+from sklearn.svm import SVC
 
