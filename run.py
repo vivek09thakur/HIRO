2200fc071cc48c92a8c26660e52371a978d935d8
Vivek Thakur <20509.vivek@gpranchi.org>
2023-08-22 22:14:48 +0530

    commit

--- run.py
+++ run.py
@@ -27,19 +27,18 @@ if __name__=='__main__':
         result = hiro.get_user_problem(user_problem)
         
         if result[0] == 1:
-            print(f'\nsearches related to {result[1][0]} :')
+            print(f'\nsearches related to {result[1][0]} :')\   
             for item_number , item_name in enumerate(result[1]):
-                print(f'{item_number} ) {item_name}')
-            
+                print(f'{item_number} ) {item_name}')   
             if item_number !=0:
-                try:
-                    confidence_input = int(input(f'select an option from 1 - {item_number} : '))
-                except Exception:
-                    print("\nplease enter a valid input for choice")
+                    confidence_input = hiro.get_choice(
+                        input(f'select an option from 1 - {item_number} : '),
+                        'please enter choice in digits (0-9)!')      
             else:
                 confidence_input = 0
-                
             disease_input = result[1]
             break
         else:
-            print('please enter a valid symptoms')
+            print('please enter a valid symptoms')     
+        num_days = hiro.get_choice(int(input("\nOkay. From how many days ? : ")),
+                                   'please enter number of days in digits (0-9)!')
