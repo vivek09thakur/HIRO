9198bf9a91e234632312187c8e167830904f568f
Vivek Thakur <20509.vivek@gpranchi.org>
2023-08-24 15:26:38 +0530

    commit

--- run.py
+++ run.py
@@ -16,28 +16,33 @@ hiro = HIRO(
     datasets[4]
 )
 hiro.prepare()
-hiro.introduce(patient_name=input('[patient name] :: '))
+hiro.introduce(
+    patient_name=input('[patient name] :: ')
+    )
     
 while True:
     user_problem = input('\n[can you explain your problem] :: ')
     result = hiro.get_user_problem(user_problem)
-    
+    # print(result)
     if result[0] == 1:
         print(f'\nsearches related to {result[1][0]} :')
         for item_number , item_name in enumerate(result[1]):
             print(f'{item_number} ) {item_name}')   
+            
         if item_number !=0:
                 confidence_input = hiro.get_choice(
                     input(f'select an option from 1 - {item_number} : '),
                    'please enter choice in digits (0-9)!') 
         else:
             confidence_input = 0
-        disease_input = result[1]
+        disease_input = result[1]  
     else:
         print('please enter a valid symptoms')      
     num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),
                                'please enter number of days in digits (0-9)!')
+    # print(num_days)
     given_symptoms = hiro.recurse(0,1,disease_input[0],result[2])
+    # print(given_symptoms)
     print(
         '\nOkay Now I am going to ask you some question,please answer all of them in yes or no'
     )
@@ -47,4 +52,6 @@ while True:
         if choice == 'yes':
             symptoms_exp.append(symps)
     second_prediction = hiro.second_prediction(symptoms_exp)
-    print(second_prediction)
+    # print(second_prediction)
+    patient_condition = hiro.calcCondition(symptoms_exp,num_days)
+    # print(patient_condition)
