2f661ee1ddb8eadaff65bedded329f8aea5d1dbe
Vivek Thakur <20509.vivek@gpranchi.org>
2023-08-24 14:53:00 +0530

    commit

--- run.py
+++ run.py
@@ -22,7 +22,6 @@ while True:
 
     user_problem = input('\n[can you explain your problem] :: ')
     result = hiro.get_user_problem(user_problem)
-    # print(result)
     
     if result[0] == 1:
         print(f'\nsearches related to {result[1][0]} :')
@@ -33,7 +32,7 @@ while True:
                 confidence_input = hiro.get_choice(
                     input(f'select an option from 1 - {item_number} : '),
                    'please enter choice in digits (0-9)!') 
-                # print(confidence_input)   
+
         else:
             confidence_input = 0
         disease_input = result[1]
@@ -41,6 +40,16 @@ while True:
         print('please enter a valid symptoms')     
     num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),
                                'please enter number of days in digits (0-9)!')
-    # print(num_days)
+
     given_symptoms = hiro.recurse(0,1,disease_input[0],result[2])
-    print(given_symptoms)
+
+    print(
+        '\nOkay Now I am going to ask you some question , please answer all of them in yes or no \n'
+        )
+    symptoms_exp = []
+    for symps in given_symptoms:
+        choice = input(f'\nAre you experiencing any {symps} ? \n enter [yes or no] :: ')
+        if choice == 'yes':
+            symptoms_exp.append(symps)
+    second_prediction = hiro.second_prediction(symptoms_exp)
+    print(second_prediction)
