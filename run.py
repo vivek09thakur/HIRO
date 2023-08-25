42fa8bbfae71d4ac65cdb9ba94b0294c492f6427
Vivek Thakur <20509.vivek@gpranchi.org>
2023-08-25 20:51:40 +0530

    commit

--- run.py
+++ run.py
@@ -16,10 +16,13 @@ hiro = HIRO(params[0],
 )
 hiro.prepare()
 hiro.introduce(patient_name=input('[patient name] :: '))
+description_list = hiro.getDescription()
+precaution_list = hiro.getPrecaution()
     
 while True:
     user_problem = input('\n[can you explain your problem] :: ')
     result = hiro.get_user_problem(user_problem)
+    
     if result[0] == 1:
         print(f'\nsearches related to {result[1][0]} :')
         for item_number , item_name in enumerate(result[1]):
@@ -32,13 +35,13 @@ while True:
         else:
             confidence_input = 0
         disease_input = result[1]
+        
     else:
         print('please enter a valid symptoms')      
-    num_days = hiro.get_choice(
-        input("\nOkay. From how many days ? : "),
+    num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),
         'please enter number of days in digits (0-9)!'
     )
-    given_symptoms = hiro.recurse(0,1,disease_input[0],result[2])
+    given_symptoms,first_prediction = hiro.recurse(0,1,disease_input[0],result[2])
     print(
         '\nOkay Now I am going to ask you some question,please answer all of them in yes or no'
     )
@@ -51,3 +54,10 @@ while True:
     
     second_prediction = hiro.second_prediction(symptoms_exp)
     patient_condition = hiro.calcCondition(symptoms_exp,num_days)
+    
+    if first_prediction[0]==second_prediction[0]:
+        print(f'\nYou may have {first_prediction[0]}\nDISEASE DESCRIPTION : {description_list[first_prediction[0]]}')
+    else:
+        print(f'\nYou may have {first_prediction[0]} or {second_prediction[0]}')
+        print(f'DISEASE DESCRIPTION [1] : {description_list[first_prediction[0]]}\nDISEASE DESCRIPTION [2] : {description_list[second_prediction[0]]}')
+        
