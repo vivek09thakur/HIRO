28c61ca08f5d5993139cc6d8416fbb65de72b6ac
Vivek Thakur <20509.vivek@gpranchi.org>
2023-08-26 02:14:47 +0530

    code refractored about 90%

--- run.py
+++ run.py
@@ -17,15 +17,17 @@ hiro = HIRO(params[0],
 hiro.prepare()
 hiro.introduce(patient_name=input('[patient name] :: '))
 description_list = hiro.getDescription()
-precaution_list = hiro.getPrecaution()
+precaution_dict = hiro.getPrecaution()
     
 while True:
     user_problem = input('\n[can you explain your problem] :: ')
     result = hiro.get_user_problem(user_problem)
+    
     if result[0] == 1:
         print(f'\nsearches related to {result[1][0]} :')
         for item_number , item_name in enumerate(result[1]):
-            print(f'{item_number} ) {item_name}')   
+            print(f'{item_number} ) {item_name}')
+              
         if item_number !=0:
                 confidence_input = hiro.get_choice(
                     input(f'select an option from 1 - {item_number} : '),
@@ -33,13 +35,13 @@ while True:
         else:
             confidence_input = 0
         disease_input = result[1]
+        
     else:
         print('please enter a valid symptoms')
         
     num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),
                                'please enter number of days in digits (0-9)!')
     given_symptoms,first_prediction = hiro.recurse(0,1,disease_input[0],result[2])
-  
     print(
         '\nOkay Now I am going to ask you some question,please answer all of them in yes or no'
     )
@@ -49,12 +51,19 @@ while True:
             f'\nAre you experiencing any {symps} ? \nEnter your answer in [yes or no] :: ')
         if choice == 'yes':
             symptoms_exp.append(symps)
+            
     second_prediction = hiro.second_prediction(symptoms_exp)
     patient_condition = hiro.calcCondition(symptoms_exp,num_days)
+    
     print(patient_condition[1])
+    
     if first_prediction[0]==second_prediction[0]:
         print(f'\nYou may have {first_prediction[0]}\nDISEASE DESCRIPTION : {description_list[first_prediction[0]]}')
     else:
         print(f'\nYou may have {first_prediction[0]} or {second_prediction[0]}')
         print(f'DISEASE DESCRIPTION [1] : {description_list[first_prediction[0]]}\nDISEASE DESCRIPTION [2] : {description_list[second_prediction[0]]}')
     
+    precaution_list = precaution_dict[first_prediction[0]]
+    
+    for precaution_num,precaution_discription in enumerate(precaution_list):
+        print(f'{precaution_num} -- {precaution_discription}')
