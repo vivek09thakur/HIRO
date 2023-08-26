d9b7a6f07cc7aa7c71859eb99e07bb2700994b88
Vivek Thakur <20509.vivek@gpranchi.org>
2023-08-26 13:36:17 +0530

    commit

--- run.py
+++ run.py
@@ -7,10 +7,6 @@ params = [
     './dataset/main/symptom_precaution.csv' ,# precaution_data
     './dataset/main/symptom_Description.csv' # description_dat
 ]
-messages = [
-    'please enter choice in digits (0-9)!', # error message 1
-    'please enter number of days in digits (0-9)!', # error message 2
-]
 
 hiro = HIRO(params[0],params[1])
 hiro.introduce(patient_name=input('[patient name] :: '))
@@ -28,23 +24,22 @@ while True:
         for item_number , item_name in enumerate(result[1]):
             print(f'{item_number} ) {item_name}')
         if item_number !=0:
-                confidence_input = hiro.get_choice(
-                    input(f'select an option from 1 - {item_number} : '),
-                    messages[0]) 
+                confidence_input = hiro.get_choice(input(f'select an option from 1 - {item_number} : '),
+                    'please enter choice in digits (0-9)!') 
         else:
             confidence_input = 0
         disease_input = result[1]
     else:
         print('please enter a valid symptoms')
         
-    num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),messages[1])
+    num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),
+                    'please enter number of days in digits (0-9)!')
     given_symptoms,first_prediction = hiro.recurse(0,1,disease_input[0],result[2])
     
     print('\nOkay Now I am going to ask you some question,please answer all of them in yes or no')
     symptoms_exp = []
     for symps in given_symptoms:
-        choice = input(
-            f'\nAre you experiencing any {symps} ? \nEnter your answer in [yes or no] :: ')
+        choice = input(f'\nAre you experiencing any {symps} ? \nEnter your answer in [yes or no] :: ')
         if choice == 'yes':
             symptoms_exp.append(symps)
             
