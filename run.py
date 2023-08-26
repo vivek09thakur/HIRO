69a07c44000ba34b1b9ee29602f4c30b9ef639fb
Vivek Thakur <20509.vivek@gpranchi.org>
2023-08-26 13:50:05 +0530

    commit

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ precaution_dict = hiro.getPrecaution(params[3])
 while True:
     user_problem = input('\n[can you explain your problem] :: ')
     result = hiro.get_user_problem(user_problem)
+    
     if result[0] == 1:
         print(f'\nsearches related to {result[1][0]} :')
         for item_number , item_name in enumerate(result[1]):
@@ -30,9 +31,13 @@ while True:
         disease_input = result[1]
     else:
         print('please enter a valid symptoms')
-    num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),'please enter number of days in digits (0-9)!')
+        
+    num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),
+                    'please enter number of days in digits (0-9)!')
     given_symptoms,first_prediction = hiro.recurse(0,1,disease_input[0],result[2])
+    
     print('\nOkay Now I am going to ask you some question,please answer all of them in yes or no')
+    
     symptoms_exp = []
     for symps in given_symptoms:
         choice = input(f'\nAre you experiencing any {symps} ? \nEnter your answer in [yes or no] :: ')
@@ -41,16 +46,21 @@ while True:
     second_prediction = hiro.second_prediction(symptoms_exp)
     patient_condition = hiro.calcCondition(symptoms_exp,num_days)
     print(f'PATIENT CONDITION -- {patient_condition[1]}')
+    
     try:
         if first_prediction[0]==second_prediction[0]:
-            print(f'''You may have {first_prediction[0]}
-                  DISEASE DESCRIPTION -- {description_list[first_prediction[0]]}''')
+            print(f'''
+                  You may have {first_prediction[0]}
+                  DISEASE DESCRIPTION -- {description_list[first_prediction[0]]}
+            ''')
         else:
             print(f'\nYou may have {first_prediction[0]} or {second_prediction[0]}')
-            print(f'''DISEASE DESCRIPTION [1] -- {description_list[first_prediction[0]]}
-                DISEASE DESCRIPTION [2] -- {description_list[second_prediction[0]]}''')
+            print(f'''
+                DISEASE DESCRIPTION [1] -- {description_list[first_prediction[0]]}
+                DISEASE DESCRIPTION [2] -- {description_list[second_prediction[0]]}
+            ''')
             precaution_list = precaution_dict[first_prediction[0]]
             for precaution_num,precaution_discription in enumerate(precaution_list):
                 print(f'{precaution_num} -- {precaution_discription}')
     except Exception as e:
-        print(f'ERROR OCCURED : {e}')
+        print('ERROR OCCURED : {}'.format(e))
