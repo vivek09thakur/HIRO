5cf6c6e6aa66a3a81b7207fa717fb934191a63aa
Vivek Thakur <20509.vivek@gpranchi.org>
2023-09-15 16:32:03 +0530

    commit

--- main.py
+++ main.py
@@ -19,10 +19,13 @@ precaution_dict = hiro.getPrecaution(params[3])
 
 
 if __name__ == '__main__':
+    
+    # KEEP ASKING USER FOR PROBLEM UNTIL HE/SHE ENTERS A VALID SYMPTOMS
     while True:
         user_problem = input('\n[can you explain your problem] :: ')
         result = hiro.get_user_problem(user_problem)
 
+        # IF RESULT IS 1 THEN IT MEANS THAT USER HAS ENTERED A VALID SYMPTOMS
         if result[0] == 1:
             print(f'\nsearches related to {result[1][0]} :')
             for item_number , item_name in enumerate(result[1]):
@@ -35,31 +38,29 @@ if __name__ == '__main__':
             disease_input = result[1]
         else:
             print('please enter a valid symptoms')
-
+            
+        
+        # IF RESULT IS 2 THEN IT MEANS THAT USER HAS ENTERED A VALID DISEASE
         num_days = hiro.get_choice(input("\nOkay. From how many days ? : "),
-                        'please enter number of days in digits (0-9)!')
+                                   'please enter number of days in digit (0-9!')
         given_symptoms,first_prediction = hiro.recurse(0,1,disease_input[0],result[2])
-
         print('\nOkay Now I am going to ask you some question,please answer all of them in yes or no')
-
         symptoms_exp = []
         for symps in given_symptoms:
             choice = input(f'\nAre you experiencing any {symps} ? \nEnter your answer in [yes or no] :: ')
-            
             if choice == 'yes':
                 symptoms_exp.append(symps) 
                 
+        
+        # START PREDICTING THE DISEASE
         second_prediction = hiro.second_prediction(symptoms_exp)
         patient_condition = hiro.calcCondition(symptoms_exp,num_days)
-        
         hiro.show_patient_condition(patient_condition[1])
-
         try:
             if first_prediction[0]==second_prediction[0]:
                 hiro.show_first_prediction(first_prediction[0],description_list)
             else:
                 hiro.show_second_prediction(first_prediction[0],second_prediction[0],description_list)
-            
                 precaution_list = precaution_dict[first_prediction[0]]
                 for precaution_num,precaution_discription in enumerate(precaution_list):
                     print(f'{precaution_num} -- {precaution_discription}')
@@ -67,6 +68,7 @@ if __name__ == '__main__':
             print('ERROR OCCURED : {}'.format(e))
             pass
         
+        # IF USER WANTS TO CONTINUE THEN CONTINUE ELSE BREAK THE LOOP
         choice = input('To turn me off you have to say [ I am fine ] :: ')
         if choice.lower() == 'i am fine':
             break
