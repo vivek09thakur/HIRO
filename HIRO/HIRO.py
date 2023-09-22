1a278f62bdf870a96205411f0d7b4ea4e74452d3
Vivek Thakur <20509.vivek@gpranchi.org>
2023-09-23 03:14:49 +0530

    commit

---
+++ HIRO/HIRO.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import mode
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+class HEALTHCARE_CHATBOT:
+    
+    def __init__(self):
+        pass
---
+++ HIRO/__init__.py
@@ -0,0 +1,1 @@
+from .HIRO import HEALTHCARE_CHATBOT
