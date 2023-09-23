7de583ce4095bd70415102b0511b7775cece21d6
Vivek Thakur <20509.vivek@gpranchi.org>
2023-09-23 16:03:36 +0530

    commit

--- HIRO/HIRO.py
+++ HIRO/HIRO.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score
 
 class HEALTHCARE_CHATBOT:
     
