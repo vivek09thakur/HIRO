1cd54fccbca7d5861c06c531e9138afea54d7d15
Vivek Thakur <20509.vivek@gpranchi.org>
2023-09-24 22:30:34 +0530

    complete about 50%

--- HIRO/support.py
+++ HIRO/support.py
@@ -1,25 +1,47 @@
-import pandas as pd
+import csv
+import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# class support_module:
+class support:
     
-#     def __init__(self,training_data_file,test_data_csv):
-#         self.training_data_file = training_data_file
-#         self.test_data_csv = test_data_csv
-#         with open(self.training_data_file,'r') as f:
-#             reader = csv.reader(f)
-#         for 
-#         pass
-    
-#     def get_disease_from_sentence(self,sentence):
-#         pass
-l1 = []
-with open('Notebook/dataset/Testing.csv','r') as f:
-    reader = pd.read_csv(f)
-    for row in reader:
-        l1.append(row)
+    def __init__(self,csv_file_1):
+        self.csv_file_1 = csv_file_1
+        self.symptoms_list = []
+        with open(self.csv_file_1, 'r') as f:
+            self.reader = csv.reader(f)
+            for row in self.reader:
+                self.symptoms_list.append(row[0])
         
-# print(l1)
-user_input = 'itching'
-for item in l1:
-    if user_input == item:
-        print(item)
+    def normalize_symptoms_list(self):
+        self.normalized_symptoms_list = []
+        # Normalize the symptoms list : skin_rash -> skin rash
+        for i in self.symptoms_list:
+            self.normalized_symptoms_list.append(i.replace('_',' '))
+        return self.normalized_symptoms_list
+    
+    def extract_symptoms(self,sentence):
+        self.sentence = sentence
+        self.extracted_symptoms = []
+        for i in self.normalized_symptoms_list:
+            if i in self.sentence:
+                self.extracted_symptoms.append(i)
+        # Return the list of symptoms extracted from the sentence as : symptoms1,symptoms2,symptoms3
+        return ','.join(self.extracted_symptoms)
+  
+class chat_support:
+    
+    def __init__(self,pairs_file) -> None:
+        self.pairs_file = pairs_file
+        with open(self.pairs_file, 'r') as f:
+            self.pairs = json.load(f)
+        self.vectorizer = CountVectorizer()
+        
+    def get_response(self,user_input):
+        pass
+
+# Test case 
+if __name__ == '__main__':
+    s = support('Notebook/dataset/Testing.csv')
+    print(s.normalize_symptoms_list())
+    print(s.extract_symptoms('I have a skin rash and headache'))
