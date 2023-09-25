836725abac6fd69d5976df2b366fe4b8992eff4f
Vivek Thakur <20509.vivek@gpranchi.org>
2023-09-25 23:40:34 +0530

    commit

--- HIRO/support.py
+++ HIRO/support.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -9,9 +10,9 @@ class support:
         self.csv_file_1 = csv_file_1
         self.symptoms_list = []
         with open(self.csv_file_1, 'r') as f:
-            self.reader = csv.reader(f)
+            self.reader = pd.read_csv(f)
             for row in self.reader:
-                self.symptoms_list.append(row[0])
+                self.symptoms_list.append(row)
         
     def normalize_symptoms_list(self):
         self.normalized_symptoms_list = []
@@ -21,6 +22,7 @@ class support:
         return self.normalized_symptoms_list
     
     def extract_symptoms(self,sentence):
+        self.normalize_symptoms_list()
         self.sentence = sentence
         self.extracted_symptoms = []
         for i in self.normalized_symptoms_list:
@@ -49,7 +51,13 @@ class chat_support:
         return self.response
 
 # Test case 
-if __name__ == '__main__':
-    s = support('Notebook/dataset/Testing.csv')
-    print(s.normalize_symptoms_list())
-    print(s.extract_symptoms('I have a skin rash and headache'))
+# if __name__ == '__main__':
+#     s = support('Notebook/dataset/Testing.csv')
+#     print(f"TEST CASE 1 : {s.extract_symptoms('I have a skin rash and headache')}")
+#     print(f"TEST CASE 2 : {s.extract_symptoms('I am suffering from fever and headache')}")
+#     print(f"TEST CASE 3 : {s.extract_symptoms('I have a headache')}")
+#     print(f"TEST CASE 4 : {s.extract_symptoms('I have a skin rash')}")
+#     print(f"TEST CASE 5 : {s.extract_symptoms('Bro I have a skin rash and headache')}")
+#     print(f"TEST CASE 6 : {s.extract_symptoms('I am actually suffering from fever and headache')}")
+#     print(f"TEST CASE 7 : {s.extract_symptoms('I have a headache')}")
+#     print(f"TEST CASE 8 : {s.extract_symptoms('I have a stomach pain')}")
