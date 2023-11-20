65d43d3ba09d604a05c416790a80913da70bfcd4
Vivek Thakur <20509.vivek@gpranchi.org>
2023-11-20 22:31:25 +0530

    commit

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
 from HIRO.HIRO import HEALTHCARE_COMPANION
 
-TRAINING_DATASET = './Notebook/dataset/Training.csv'
-TESTING_DATASET = './Notebook/dataset/Testing.csv'
+TRAINING_DATA = './Notebook/dataset/Training.csv'
+TESTING_DATA = './Notebook/dataset/Testing.csv'
 SYMPTOM_DESCRIPTION = './Notebook/dataset/symptom_Description.csv'
-PRECAUTION_DATASET = './Notebook/dataset/precautions.csv'
-QUESTION_RESPONSE_DATASET = './Notebook/question_response_pairs.json'
+PRECAUTION_DATA = './Notebook/dataset/precautions.csv'
+CHAT_DATASET = './Notebook/question_response_pairs.json'
 
-hiro = HEALTHCARE_COMPANION(TRAINING_DATASET, TESTING_DATASET, SYMPTOM_DESCRIPTION, PRECAUTION_DATASET,
-                        QUESTION_RESPONSE_DATASET)
+hiro = HEALTHCARE_COMPANION(TRAINING_DATA, TESTING_DATA, SYMPTOM_DESCRIPTION, PRECAUTION_DATA, CHAT_DATASET)
 hiro.process_training_data(show_models_stats=True)
 hiro.build_robust_model()
 
