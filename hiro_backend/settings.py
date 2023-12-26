20b899258ed7efbf4eef0c569d2f8a028a3b6e05
Vivek Thakur <20509.vivek@gpranchi.org>
2023-12-26 15:50:20 +0530

    Update settings.py and index.html files

--- hiro_backend/settings.py
+++ hiro_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'hiro_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'HIRO_CHAT_DATA.db',
     }
 }
 
@@ -117,6 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+# settings.py
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,57 @@
 from django.shortcuts import render
 from HIRO.HIRO import HEALTHCARE_COMPANION
 
+TRAINING_DATA = './Notebook/dataset/Training.csv'
+TESTING_DATA = './Notebook/dataset/Testing.csv'
+SYMPTOM_DESCRIPTION = './Notebook/dataset/symptom_Description.csv'
+PRECAUTION_DATA = './Notebook/dataset/precautions.csv'
+CHAT_DATASET = './Notebook/QA_data.json'
+
+hiro = HEALTHCARE_COMPANION(TRAINING_DATA,TESTING_DATA,SYMPTOM_DESCRIPTION,PRECAUTION_DATA, CHAT_DATASET)
+hiro.process_training_data(show_models_stats=True)
+hiro.build_robust_model()
+
 # Create your views here.
 def Homepage(request):
+    try:
+        user_prompt = request.POST.get('user_prompt')
+        if user_prompt != None:
+            extracted_symptoms = hiro.extract_symptoms(user_prompt,show_extracted_symptoms=True)
+            if extracted_symptoms:
+                disease_predicted = hiro.predict_disease_from_symptoms(extracted_symptoms)
+                disease_description = hiro.get_description(disease_predicted['Final Prediction'])
+                disease_precaution = hiro.get_precautions(disease_predicted['Final Prediction'])
+                
+                return render(request, 'webapp/index.html', {
+                    'user_prompt': user_prompt,
+                    'disease_from_test1': disease_predicted['Random Forest'],
+                    'disease_from_test2': disease_predicted['SVC'],
+                    'disease_from_test3': disease_predicted['Naive Bayes'],
+                    'disease_description': disease_description,
+                    'disease_precaution': disease_precaution,
+                })
+                
+            else:
+                response = hiro.talk_to_user(user_prompt)
+                return render(request, 'webapp/index.html', {
+                        'user_prompt': user_prompt,
+                        'response': response,
+                        'disease_from_test1': 'None',
+                        'disease_from_test2': 'None',
+                        'disease_from_test3': 'None',
+                        'disease_description': 'None',
+                        'disease_precaution': 'None',
+                    })
+    except Exception as e:
+        print(e)
+        return render(request, 'webapp/index.html', {
+            'user_prompt': 'None',
+            'response': 'None',
+            'disease_from_test1': 'None',
+            'disease_from_test2': 'None',
+            'disease_from_test3': 'None',
+            'disease_description': 'None',
+            'disease_precaution': 'None',
+        })
+            
     return render(request, 'webapp/index.html')
